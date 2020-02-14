527235d86ff646058bf3b035c4a8fe3014e90ecb
James Ng <james.ng@projapps.com>
2020-02-14 16:40:26 +0800

    Complete Stage #2

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,7 +1,32 @@
 package parking
 
+import java.util.Scanner
+
 fun main() {
-    println("White car has parked.")
-    println("Yellow car left the parking lot.")
-    println("Green car just parked here.")
+    val spots = arrayOfNulls<String>(2)
+    spots[0] = "LS-!@-JJ-2345"
+
+    val scanner = Scanner(System.`in`)
+    val input = scanner.nextLine().split(" ")
+
+    when (input[0]) {
+        "park" -> {
+            for (i in 1..2) {
+                if (spots[i - 1] == null) {
+                    spots[i - 1] = input[1]
+                    println("${input[2]} car parked on the spot $i.")
+                    break
+                }
+            }
+        }
+        "leave" -> {
+            val i = input[1].toInt()
+            if (spots[i - 1] == null) {
+                println("There is no car in the spot $i.")
+            } else {
+                spots[i - 1] = null
+                println("Spot $i is free.")
+            }
+        }
+    }
 }
--- Parking Lot/task/test/ParkingTest.java
+++ Parking Lot/task/test/ParkingTest.java
@@ -1,35 +1,8 @@
 import org.hyperskill.hstest.v6.stage.BaseStageTest;
-import org.hyperskill.hstest.v6.testcase.CheckResult;
-import org.hyperskill.hstest.v6.testcase.TestCase;
 import parking.MainKt;
 
-import java.util.Arrays;
-import java.util.List;
-
-
-public class ParkingTest extends BaseStageTest {
+public abstract class ParkingTest<T> extends BaseStageTest<T> {
     public ParkingTest() {
         super(MainKt.class);
-    }
-
-    public List<TestCase> generate() {
-        return Arrays.asList(
-            new TestCase()
-        );
-    }
-
-    public CheckResult check(String reply, Object clue) {
-        String text =
-            "White car has parked.\n" +
-            "Yellow car left the parking lot.\n" +
-            "Green car just parked here.";
-
-        if (!reply.trim().equals(text.trim())) {
-            return CheckResult.FALSE(
-                "You printed the wrong text! " +
-                    "See the example.");
-        } else {
-            return CheckResult.TRUE;
-        }
     }
 }
