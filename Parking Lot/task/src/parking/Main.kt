61d438b6366763a0c3eb4f0575956700737cc004
James Ng <james.ng@projapps.com>
2020-02-16 13:00:53 +0800

    Complete Stage #5

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -62,6 +62,66 @@ fun main() {
                     }
                 }
             }
+            "reg_by_color" -> {
+                if (size == 0) {
+                    println("Sorry, parking lot is not created.")
+                } else if (available == size) {
+                    println("Parking lot is empty.")
+                } else {
+                    val mutable = mutableListOf<String>()
+                    for (i in 1..size) {
+                        val car = spots[i - 1]
+                        if (car != null && car.colour.toLowerCase() == input[1].toLowerCase()) {
+                            mutable.add(car.registration)
+                        }
+                    }
+                    if (mutable.isEmpty()) {
+                        println("No cars with color ${input[1]} were found.")
+                    } else {
+                        println(mutable.joinToString(", "))
+                    }
+                }
+            }
+            "spot_by_color" -> {
+                if (size == 0) {
+                    println("Sorry, parking lot is not created.")
+                } else if (available == size) {
+                    println("Parking lot is empty.")
+                } else {
+                    val mutable = mutableListOf<Int>()
+                    for (i in 1..size) {
+                        val car = spots[i - 1]
+                        if (car != null && car.colour.toLowerCase() == input[1].toLowerCase()) {
+                            mutable.add(i)
+                        }
+                    }
+                    if (mutable.isEmpty()) {
+                        println("No cars with color ${input[1]} were found.")
+                    } else {
+                        println(mutable.joinToString(", "))
+                    }
+                }
+            }
+            "spot_by_reg" -> {
+                if (size == 0) {
+                    println("Sorry, parking lot is not created.")
+                } else if (available == size) {
+                    println("Parking lot is empty.")
+                } else {
+                    var found = false
+                    for (i in 1..size) {
+                        val car = spots[i - 1]
+                        if (car != null && car.registration == input[1]) {
+                            found = true
+                            println(i)
+                            break
+                        }
+                    }
+                    if (!found) {
+                        println("No cars with registration number ${input[1]} were found.")
+                    }
+                }
+            }
             "exit" -> break@loop
         }
     }
